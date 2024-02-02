84fdd0f267d38825a9e04b84ebf19c5c5d448e74
jacquelyn bloomfield <jbloomfield16@lapu.edu>
2024-02-02 10:38:21 +0000

    server debugging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //the importing for Jacq's file
-import { ModalWindow } from "src\form.js"
+// import { ModalWindow } from "form.js"
 
-const modalWindow1 = new ModalWindow('first attempt')
+const modalWindow1 = new ModalWindow()
 document.appendChild(modalWindow1.element);
 // end of Jacq's file
